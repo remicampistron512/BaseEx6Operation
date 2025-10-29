57409fe870a51609bb1ab36d8807f9ef4203f0c1
Rémi Campistron <remi.campistron@gmail.com>
2025-10-29 14:17:15 +0100

    partie 1 à 3 de l'exercice

--- src/Operation.java
+++ src/Operation.java
@@ -2,8 +2,55 @@
 public class Operation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println(add(5,2));
+		System.out.println(sub(5,2));
+		System.out.println(mul(5,2));
+		System.out.println(div(5,0));
+		
+		triangle();
+	}
+	
+	public static int add(int a,int b) {
+		int result = a + b;
+		return result;
+	}
+	
+	public static int sub(int a,int b) {
+		int result = a - b;
+		return result;
+	}
+	
+	public static int mul(int a,int b) {
+		int result = a * b;
+		return result;
+	}
+	
+	public static float div(int a,int b) {
+		if(b != 0) {
+			float result = a / b;
+			return result;
+		} else {
+			float result = 0;
+			System.out.println("erreur");
+			return result;
+		}
+	}
+	
+	public static void triangle() {
+		int triangleHeight = 6;
+		int triangleMaxWidth = 13;
+		int [] triangleWidth = {1,3,5,7,9,11,13};
+		for (int i = 0; i < triangleHeight;i++ ) {
+			
+			for (int k=1;k<(triangleMaxWidth-i)+1 /2;k++) {
+				System.out.print(" ");
+			}
+			for (int j=0; j<triangleWidth[i];j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
 		
 	}
-
 }
