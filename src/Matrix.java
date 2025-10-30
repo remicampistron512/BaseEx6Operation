b607e09888738dfabd592dac9533d38cff40194d
Rémi Campistron <remi.campistron@gmail.com>
2025-10-30 13:06:16 +0100

    Partie 4 à 6 de l'exercice

--- src/Matrix.java
+++ src/Matrix.java
@@ -27,13 +27,55 @@ public class Matrix {
         mulScalMatrix(fourthMatrix, 2);
     }
 
-	private static void mulScalMatrix(int[][] fourthMatrix, int i) {
+	private static void mulScalMatrix(int[][] fourthMatrix, int n) {
+		int [][]resultMatrix = {{0,0,0},
+				{0,0,0},
+						{0,0,0},
+						};
 		
+			for (int i=0;i < fourthMatrix.length;i++) {
+				for (int j =0;j<fourthMatrix[i].length;j++) {
+				
+					
+						resultMatrix[i][j] = fourthMatrix[i][j] * n;
+						
+					
+				}
+			}
+		
+			for (int i=0; i < resultMatrix.length;i++) {
+				System.out.println("");
+			for (int j=0; j < resultMatrix[i].length;j++) {
+				System.out.println(resultMatrix[i][j]);
+			}
+		}
+
 		
 	}
 
 	private static void subMatrix(int[][] firstMatrix, int[][] secondMatrix) {
-		// TODO Auto-generated method stub
+		int [][]resultMatrix = {{0,0,0},
+				{0,0,0},
+						{0,0,0},
+						};
+		
+			for (int i=0;i < firstMatrix.length;i++) {
+				for (int j =0;j<firstMatrix[i].length;j++) {
+				
+					
+						resultMatrix[i][j] = firstMatrix[i][j] - secondMatrix[i][j];
+						
+					
+				}
+			}
+		
+			for (int i=0; i < resultMatrix.length;i++) {
+				System.out.println("");
+			for (int j=0; j < resultMatrix[i].length;j++) {
+				System.out.println(resultMatrix[i][j]);
+			}
+		}
+
 		
 	}
 
@@ -42,7 +84,7 @@ public class Matrix {
 								{0,0,0},
 								{0,0,0},
 								};
-		List<Integer> list = new ArrayList<>();
+		
 		for (int i=0;i < firstMatrix.length;i++) {
 			for (int j =0;j<firstMatrix[i].length;j++) {
 				
@@ -54,6 +96,7 @@ public class Matrix {
 			}
 		
 		for (int i=0; i < resultMatrix.length;i++) {
+			System.out.println("");
 			for (int j=0; j < resultMatrix[i].length;j++) {
 				System.out.println(resultMatrix[i][j]);
 			}
